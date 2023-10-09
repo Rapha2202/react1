a3bf24f396dd0c322c3e1ec5d808c8b4e3fe28de
Rapha2202~ <raphael.foulonbinet@gmail.com>
2023-10-09 22:43:03 +0200

    Ajout de pokemons et d'un bouton suivant et précédent

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import PokemonCard from './components/PokemonCard'
+import { useState } from "react";
 
 function App() {
 
@@ -9,12 +10,39 @@ function App() {
         "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
     },
     {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
       name: "mew",
     },
   ]
 
+  const [pokemonIndex, setpokemonIndex] = useState(0);
+
+  const handleClickAdd = () => {
+    setpokemonIndex(pokemonIndex + 1)
+  }
+
+  const handleClickRemove = () => {
+    setpokemonIndex(pokemonIndex - 1)
+  }
+
   return (<div>
-    <PokemonCard pokemon={pokemonList[0]} />
+    <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+    {pokemonIndex > 0 ? <button onClick={handleClickRemove}>Précédent</button> : <p></p>}
+    {pokemonIndex < pokemonList.length - 1 ? <button onClick={handleClickAdd}>Suivant</button> : <p></p>}
   </div>)
 }
 
