ba186f431972c2e143e933093da2663755db7af8
Rapha2202~ <raphael.foulonbinet@gmail.com>
2023-10-09 21:02:39 +0200

    Revert "Affichage dynamique"
    This reverts commit a898fd8f05fe1498895a6c70e6234965497b62a7.

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,20 +1,7 @@
 function PokemonCard() {
-    const pokemonList = [
-        {
-            name: "bulbasaur",
-            imgSrc:
-                "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-        },
-        {
-            name: "mew",
-        },
-    ]
-
-    const pokemon = pokemonList[0]
-
     return <figure>
-        {pokemon.imgSrc ? <img src={pokemon.imgSrc} alt="Image d'un Pokemon" /> : <p>???</p>}
-        <figcaption>{pokemon.name}</figcaption>
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Image d'un Pokemon" />
+        <figcaption>Bulbasaur</figcaption>
     </figure>
 }
 
